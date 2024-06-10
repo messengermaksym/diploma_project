9f2170ee39545bd9bf65ee7389fbb03e1271acf7
messengermaksym <maksmasenditch@gmail.com>
2024-06-10 23:32:43 +0300

    small changes

--- main_app/permissions.py
+++ main_app/permissions.py
@@ -1,13 +1,10 @@
 from rest_framework import permissions
 
-
+# Дозвіл тільки для адміністраторів, для інших тільки читання.
 class IsAdminOrReadOnly(permissions.BasePermission):
-    """
-    Дозвіл, що дозволяє лише адміністраторам редагувати об'єкти.
-    """
 
     def has_permission(self, request, view):
-        # Дозвіл тільки для адміністраторів, для інших тільки читання.
+
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_staff
